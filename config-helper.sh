fba55a5604ce08ead70fccf79e64024b3e5094df
manishas <manisha@shippable.com>
2021-02-08 23:00:28 -0800

    initial commit for pythonn sample

--- config-helper.sh
+++ config-helper.sh
@@ -5,6 +5,8 @@ cat <<EOF > ~/.pypirc
 index-servers = local
 [local]
 repository: https://pipedemo.jfrog.io/artifactory/api/pypi/python-remote
+username: $ARTIFACTORY_USERNAME
+password: $ARTIFACTORY_PASSWORD
 EOF
 
 echo "Created .pypirc file: Here it is: "
